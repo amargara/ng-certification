9d1c2d8246069c75281fff38b902fceb0ce57555
Ana Albuquerque <>
2021-04-05 15:36:54 +0100

    fix missing files in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
