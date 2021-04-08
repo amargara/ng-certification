06a85ff38c1b25629a556bdfb95fe3cdfc008474
Ana Albuquerque <>
2021-04-08 22:31:42 +0100

    remove unused imports

--- src/app/weather-forecast/add-location/zip-code/zip-code.component.ts
+++ src/app/weather-forecast/add-location/zip-code/zip-code.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { DataService } from '../../../core/data.service';
--- src/app/weather-forecast/five-day-weather-location/five-day-weather-location.component.ts
+++ src/app/weather-forecast/five-day-weather-location/five-day-weather-location.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Route } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { DataService } from '../../core/data.service';
